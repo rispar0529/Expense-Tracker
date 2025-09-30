c887595b6c21e45433ffe7f0a0b521a1d4f697c5
rispar0529 <rishabhparihar511@gmail.com>
2025-10-01 00:03:09 +0530

    Minor changes done
    Signed-off-by: rispar0529 <rishabhparihar511@gmail.com>

--- backend/models/User.js
+++ backend/models/User.js
@@ -5,7 +5,7 @@ const bcrypt = require("bcryptjs");
 
 const UserSchema = new mongoose.Schema(
     {
-        fullName: {type: String, requires: true},
+        fullName: {type: String, required: true},
         email: {type: String, required: true, unique: true },
         password: { type: String, required:true },
         profileImageUrl: {type: String, default: null}
